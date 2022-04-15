38edfb5a2f12e899bd0d0224bf528fb6beed62b3
utku-guclu <stanger307@live.com>
2022-04-15 16:16:43 -0700

    styles added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,14 +5,14 @@ export default function Product(props) {
         <input onClick={props.handleCheck} type="checkbox"></input>
         <div className="product-info">
           <ul>
-            <li>{props.sku}</li>
-            <li>{props.name}</li>
-            <li>{props.price}</li>
-            <li>{props.size}</li>
-            <li>{props.height}</li>
-            <li>{props.width}</li>
-            <li>{props.len}</li>
-            <li>{props.weight}</li>
+            <li>{props.sku && props.sku} </li>
+            <li>{props.name && "Name: " + props.name}</li>
+            <li>{props.price && "Price: " + props.price}</li>
+            <li>{props.size && "Size: " + props.size}</li>
+            <li>{props.height && "Height: " + props.height}</li>
+            <li>{props.width && "Width: " + props.width}</li>
+            <li>{props.len && "Length: " + props.len}</li>
+            <li>{props.weight && "Weight: " + props.weight}</li>
           </ul>
         </div>
       </div>
