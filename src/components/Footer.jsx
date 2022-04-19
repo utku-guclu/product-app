509bd19635b57c024355049476a4364c5fadcf75
Utku <34550887+utku-guclu@users.noreply.github.com>
2022-04-19 16:10:06 +0300

    Create Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <footer>
-        <p>Scandiweb Test assignment - &copy;Utku Guclu - 2022 </p>
+        <p>Product App - &copy;Utku Guclu - 2022 </p>
       </footer>
     </div>
   );
